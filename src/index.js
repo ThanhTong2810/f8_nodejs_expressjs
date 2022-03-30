b9a87336b256010932044e6d60e6cd26ca0d614a
ThanhTong2810 <tong.duong@kthub.dev>
2022-03-30 14:17:51 +0700

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -17,13 +24,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Routing init
+route(app);
 
+// Listen port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
